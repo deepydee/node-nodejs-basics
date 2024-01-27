a64644226f7a21ddf84e4cb0e16bd01e5baaa9b5
Vitaliy Semenov <synthex@bk.ru>
2024-01-27 21:07:48 +0600

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
+import { existsSync, rm } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        if (!existsSync(filePath)) {
+          throw new Error('FS operation failed');
+        }
+
+        rm(filePath, { recursive: true }, (err) => {
+            if (err) throw err;
+            console.log('Removed!');
+        });
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await remove();
