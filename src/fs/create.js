b9e961dbcdf1cf2dff96d6845588dd722d221f6f
Vitaliy Semenov <synthex@bk.ru>
2024-01-27 20:31:56 +0600

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { existsSync, promises as fsPromises } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fresh.txt');
+
+    try {
+      if (existsSync(filePath)) {
+        throw new Error('FS operation failed');
+      }
+
+      await fsPromises.writeFile(filePath, 'I am fresh and young');
+    } catch (err) {
+      console.error(err);
+    }
 };
 
 await create();
