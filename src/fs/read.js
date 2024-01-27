9374331bd397e21847434bea1b0d9da7940d2ba0
Vitaliy Semenov <synthex@bk.ru>
2024-01-27 21:19:33 +0600

    feat: implement read,js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import { existsSync, readFile } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        if (!existsSync(filePath)) {
+          throw new Error('FS operation failed');
+        }
+
+        readFile(filePath, 'utf8', (err, data) => {
+            if (err) throw err;
+
+            console.log(data);
+        });
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await read();
