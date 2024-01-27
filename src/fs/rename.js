3549264d63d7d7bf2dd652cc3fe920f5faf3d43d
Vitaliy Semenov <synthex@bk.ru>
2024-01-27 21:05:01 +0600

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import { existsSync, rename as fsRename } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    const src = join(__dirname, 'files', 'wrongFilename.txt');
+    const dest = join(__dirname, 'files', 'properFilename.md');
+
+    try {
+        if (!existsSync(src)) {
+          throw new Error('FS operation failed');
+        }
+
+        if (existsSync(dest)) {
+          throw new Error('FS operation failed');
+        }
+
+        fsRename(src, dest, (err) => {
+            if (err) throw err;
+            console.log('Renamed!');
+        });
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await rename();
