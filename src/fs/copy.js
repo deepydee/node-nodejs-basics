019ed464aeff1960aab9996c8f1e1055931487d7
Vitaliy Semenov <synthex@bk.ru>
2024-01-27 20:58:17 +0600

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import { existsSync, cp } from 'fs';
+import { mkdir } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const src = join(__dirname, 'files');
+    const dest = join(__dirname, 'files_copy');
+
+    try {
+        if (existsSync(dest)) {
+          throw new Error('FS operation failed');
+        }
+
+        await mkdir(dest);
+        cp(src, dest, { recursive: true }, (err) => {
+            if (err) throw err;
+            console.log('Copied!');
+        });
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await copy();
