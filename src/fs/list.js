69f919a50c35235f3adabf09d803e280a6683471
Vitaliy Semenov <synthex@bk.ru>
2024-01-27 21:13:21 +0600

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import { existsSync, readdir } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const dir = join(__dirname, 'files');
+
+    try {
+        if (!existsSync(dir)) {
+          throw new Error('FS operation failed');
+        }
+
+        const files = readdir(dir, (err, files) => {
+            if (err) throw err;
+
+            console.log(files);
+        });
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await list();
