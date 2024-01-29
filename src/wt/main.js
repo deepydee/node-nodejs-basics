124321faba56d1d9dffb7e60bcd775043b34ec46
Vitaliy Semenov <synthex@bk.ru>
2024-01-29 11:43:20 +0600

    feat: implement worker.js and thread.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const numWorkers = os.cpus().length;
+    const workers = [];
+    const promises = [];
+
+    for(let i = 0; i < numWorkers; i++) {
+        const worker = new Worker(new URL('./worker.js', import.meta.url));
+        workers.push(worker);
+
+        const data = 10 + i;
+        worker.postMessage(data);
+
+        const promise = new Promise((resolve, reject) => {
+            worker.on('message', (result) => {
+                resolve({ status: 'resolved', data: result });
+                worker.terminate();
+            });
+            worker.on('error', reject);
+            worker.on('exit', (code) => {
+                if (code !== 0) {
+                    reject(new Error(`Worker stopped with exit code ${code}`));
+                }
+            });
+        });
+
+        promises.push(promise);
+    }
+
+    try {
+        const results = await Promise.all(promises);
+        console.log(results);
+    } catch (err) {
+        console.error(err);
+    }
 };
 
-await performCalculations();
+await performCalculations().catch(console.error);
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
+import { parentPort } from 'worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
+const sendResult = (result) => {
     // This function sends result of nthFibonacci computations to main thread
+    parentPort.postMessage(result);
 };
 
-sendResult();
+parentPort.on('message', (n) => {
+    const result = nthFibonacci(n);
+
+    sendResult(result);
+});
