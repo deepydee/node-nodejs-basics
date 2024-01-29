57dbf5a1243314cb90586c50d85742300ae71ab3
Vitaliy Semenov <synthex@bk.ru>
2024-01-29 10:33:03 +0600

    feat: implement decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { createGunzip } from 'node:zlib';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const inFile = join(__dirname, 'files', 'archive.gz');
+const outFile = join(__dirname, 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+    pipeline(
+        createReadStream(inFile),
+        createGunzip(),
+        createWriteStream(outFile),
+        (err) => {
+            if (err) {
+              console.error('Pipeline failed', err);
+            } else {
+              console.log('File decompressed successfully');
+            }
+        }
+      );
 };
 
 await decompress();
