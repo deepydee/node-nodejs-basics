9aba6a40a58a15f7bde0e3d5e7369c328f127853
Vitaliy Semenov <synthex@bk.ru>
2024-01-29 11:55:18 +0600

    feat: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,27 @@
+import { spawn } from 'node:child_process';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+     // Create a child process with IPC enabled
+    const child = spawn('node', [join(__dirname, 'files', 'script.js'), ...args], {
+        stdio: ['pipe', 'pipe', process.stderr, 'ipc']
+    });
+
+    // Forward stdin from the master process to the child process
+    process.stdin.pipe(child.stdin);
+
+    // Forward stdout from the child process to the master process stdout
+    child.stdout.pipe(process.stdout);
+
+    // Handle child process exit
+    child.on('exit', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
