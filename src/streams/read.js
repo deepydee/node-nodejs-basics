69cce795f7c37344356fd4bad23295f2bda1a976
Vitaliy Semenov <synthex@bk.ru>
2024-01-29 10:15:59 +0600

    feat: implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'node:fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const readStream = createReadStream(filePath);
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    await new Promise((resolve, reject) => {
+        readStream.on('end', resolve);
+        readStream.on('error', reject);
+    });
 };
 
 await read();
