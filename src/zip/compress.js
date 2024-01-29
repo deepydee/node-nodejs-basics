864fd9ec9231332cc2f4d75369ded5411864880d
Vitaliy Semenov <synthex@bk.ru>
2024-01-29 10:29:05 +0600

    feat: implement compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { createGzip } from 'node:zlib';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const inFile = join(__dirname, 'files', 'fileToCompress.txt');
+const outFile = join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    pipeline(
+        createReadStream(inFile),
+        createGzip(),
+        createWriteStream(outFile),
+        (err) => {
+            if (err) {
+              console.error('Pipeline failed', err);
+            } else {
+              console.log('Pipeline succeeded');
+            }
+        }
+      );
+
+      console.log('File compressed successfully');
 };
 
 await compress();
