1564ccc97f38f95af1e816fc5c5a4ad730cd0fda
Vitaliy Semenov <synthex@bk.ru>
2024-01-29 12:00:10 +0600

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rssEnvVariables = Object.entries(process.env)
+        .filter(([key, _]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`);
+
+    const formattedEnvVariables = rssEnvVariables.join('; ');
+
+    console.log(formattedEnvVariables);
 };
 
 parseEnv();
