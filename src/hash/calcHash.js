8c419399fb3e739477396d0b98bdbbd4dc2b5912
Vitaliy Semenov <synthex@bk.ru>
2024-01-29 10:45:50 +0600

    feat: implement cahcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hashStream = createHash('sha256');
+
+    createReadStream(filePath)
+        .pipe(hashStream);
+
+    hashStream.on('finish', () => {
+        console.log('Hash:', hashStream.read().toString('hex'));
+    });
+
+    await new Promise((resolve, reject) => {
+        hashStream.on('error', reject);
+        hashStream.on('finish', resolve);
+    });
+
+    hashStream.end();
 };
 
 await calculateHash();
